be99beab8e77a14aa9b663ad7a2dddebbe49bfb3
Thomas Torsney-Weir <torsneyt@gmail.com>
2020-01-16 15:27:37 +0000

    add mouse tracking dataset

---
+++ sa_mouse_tracking/combine_study_data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+jq -s add study_results_*.json | tee \
+  study_results.all.json \
+  >(jq '[.[] | ."mouseevents-events"[]]' > mouse_events.json) \
+  | jq '[.[] | del(."mouseevents-events")]' > portfolio_selections.json
